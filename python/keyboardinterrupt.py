a64083ddcae11de1f7e0ccb4053e5e96c0c2e592
mzaprudin <mzaprudin@gmail.com>
2022-11-16 07:15:53 +0000

    Initial commit First hits is added: KeyboardInterrupt

---
+++ python/keyboardinterrupt.py
@@ -0,0 +1,17 @@
+"""
+This example demostrates how to catch keyboard interruption
+and finish program correctly
+"""
+
+def main():
+    try:
+        print('Press Ctrl+C for exit...')
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print('KeyboardInterrupt')
+
+if __name__ == '__main__':
+    print('start')
+    main()
+    print('done')
