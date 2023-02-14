a6163596c07e32472ede024279e3a68b4b411a41
mzaprudin <mzaprudin@gmail.com>
2023-02-14 17:34:54 +0000

    [UPD] Cpu tools install

--- linux/cpu.sh
+++ linux/cpu.sh
@@ -1,6 +1,11 @@
+sudo apt install -y linux-tools-$(uname -r)
+
 lscpu
 
 cat /proc/cpuinfo | grep "MHz"
 
 sudo cpupower frequency-set -g performance
 # sudo cpupower frequency-set -g powersave
+
+sudo systemctl enable cpupower
+sudo systemctl start cpupower
