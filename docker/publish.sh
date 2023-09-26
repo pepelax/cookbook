86859bc1e3fde745b863acf741213d622982e766
mzaprudin <mzaprudin@gmail.com>
2023-09-26 10:18:15 +0500

    [NEW] Docker publish

---
+++ docker/publish.sh
@@ -0,0 +1,7 @@
+sudo docker login
+
+sudo docker images
+
+sudo docker tag <imagename> <username>/<imagenamealt>:<version marker like latest>
+
+sudo docker push <username>/<imagenamealt>:<version marker like latest>
