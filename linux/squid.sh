ef82b7c0d6411e652e38de18fb498f0303eec2bf
mzaprudin <mzaprudin@gmail.com>
2024-09-16 18:56:58 +0500

    [NEW] squid.sh

---
+++ linux/squid.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install squid
+sudo apt install apache2-utils
+
+# Create a password file:
+sudo touch /etc/squid/passwd
+sudo chown proxy:proxy /etc/squid/passwd
+sudo chmod 640 /etc/squid/passwd
+
+# Add users to the password file:
+sudo htpasswd -c /etc/squid/passwd username
+
+sudo systemctl restart squid
+sudo ufw allow 3128/tcp
